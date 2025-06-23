5e810e164370ba842ff0faa67e20c88b23111f00
Darren Borromeo <darrenborromeodrb@gmail.com>
2025-06-23 22:58:06 +1000

    added double jump

--- components/jump_component/jump_component.gd
+++ components/jump_component/jump_component.gd
@@ -4,66 +4,47 @@ class_name JumpComponent
 @export var actor: CharacterBody2D
 @export var jump_force: float = -200
 @export var coyote_time: float = 0.2
-@onready var coyote_timer = $CoyoteTimer
+@export var total_jumps: int = 2  # Default to double jump
 
-var can_jump: bool = false
+@onready var coyote_timer = $CoyoteTimer
 @onready var buffer_timer = $BufferTimer
+
+var jumps := total_jumps
+var can_jump_from_ground := false
 
 signal jumped
 
-
 func _ready():
-	if not actor: set_physics_process(false)
+	if not actor:
+		set_physics_process(false)
 
 
-## NOTE: This MUST be called AFTER move_and_slide()
-"""
-	Requirements: 
-		This must be called in the physics function AFTER move_and_slide()
-		This handles the logic that determines if a player can jump.
-		It handles coyote time and jump buffers. 
-"""
+## Call this AFTER move_and_slide()
 func handle_jump():
-	if actor.is_on_floor() and not can_jump:
-		can_jump = true
+	if actor.is_on_floor():
+		can_jump_from_ground = true
+		jumps = total_jumps
 	
-	# We have left the floor. 
 	if not actor.is_on_floor() and coyote_timer.is_stopped():
 		coyote_timer.start(coyote_time)
 	
-	# Buffer timer for jump.
-	if not buffer_timer.is_stopped(): 
+	if not buffer_timer.is_stopped():
 		try_to_jump()
-		
-			
+
 func _on_coyote_timer_timeout():
-	can_jump = false 
+	can_jump_from_ground = false
 
 
-"""
-	The player is attempting to jump. 
-	return: 
-		true -> Jump is successful
-		false -> Jump is unsuccessful
-		Use these return values if you want to transition into another state in a state machine. 
-	signal: signal that the player has successfully jumped. 
-"""
-func try_to_jump():	
-	# Wants to jump.
-	if can_jump: 
-		can_jump = false
-		coyote_timer.stop() 		
+func try_to_jump():
+	if jumps > 0:
+		if actor.is_on_floor() or can_jump_from_ground:
+			can_jump_from_ground = false
+			coyote_timer.stop()
+
+		jumps -= 1
 		buffer_timer.stop()
 		actor.velocity.y = jump_force
-		
 		jumped.emit()
 	
-	# Start the buffer timer. 
-	# If the actor hits the ground while the buffer timer is running, it will attempt to jump. 
-	# See handle_jump above. 
-	elif buffer_timer.is_stopped(): 
+	elif buffer_timer.is_stopped():
 		buffer_timer.start()
-	
-
-func second_jump(): 
-	actor.velocity.y = jump_force
